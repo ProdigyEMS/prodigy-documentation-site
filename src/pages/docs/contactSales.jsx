ea55dc5507b93bcfacbcdffe5a25e30265587f37
jdiclemente <james@prodigyems.com>
2026-03-26 19:36:06 -0400

    added book a demo to contact sales page

--- src/pages/docs/contactSales.jsx
+++ src/pages/docs/contactSales.jsx
@@ -1,21 +1,46 @@
-import React from "react";
-import HubspotForm from "react-hubspot-form";
-import Link from 'next/link'
+import React, { useEffect } from "react";
 
 
-export default function App() { 
+export default function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js";
+    script.type = "text/javascript";
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="salesform">
-    <div className="formtext">
-      <p>Our sales team is always happy to answer any questions you might have. If you provide us with the total number of clinicians you have on staff, we can supply you with a quote to get us started. Looking to a demo? Use this calendar link to <Link href="https://meetings.hubspot.com/ryan1574">schedule your demo</Link> today.</p>
-    </div>
-      <HubspotForm
-        portalId={process.env.NEXT_PUBLIC_PORTALID}
-        formId={process.env.NEXT_PUBLIC_FORMID}
-        onSubmit={() => console.log("Submit!")}
-        onReady={(form) => console.log("Form ready!")}
-        loading={<div></div>}
-      />
+
+      {/* Book a Demo */}
+      <div className="salesSection">
+        <h2>Book a Demo</h2>
+        <p>
+          See Prodigy EMS in action. Schedule a personalized demo with one of our
+          sales representatives and learn how Prodigy can streamline your agency's
+          operations from dispatch to billing.
+        </p>
+        <div
+          className="meetings-iframe-container"
+          data-src="https://go.prodigyems.com/meetings/jamescal/demo-on-site?embed=true"
+        />
+      </div>
+
+      {/* Contact Sales */}
+      <div className="salesSection">
+        <h2>Contact Sales</h2>
+        <p>
+          Have questions or want a custom quote? Fill out the form below and a
+          member of our sales team will be in touch shortly. If you can include
+          the number of clinicians on staff, we can prepare a tailored pricing
+          estimate for your agency.
+        </p>
+        {/* Form will go here */}
+      </div>
+
     </div>
   );
 }
